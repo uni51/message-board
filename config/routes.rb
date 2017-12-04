2f65ab26aabb66d45f1a095e386d05b6b173bad0
Koichi Shimomura <unieye51@gmail.com>
2017-12-04 08:34:18 +0000

    CRUD messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # # CRUD
+  # get 'messages/:id', to: 'messages#show'
+  # post 'messages', to: 'messages#create'
+  # put 'messages/:id', to: 'messages#update'
+  # delete 'messages/:id', to: 'messages#destroy'
+
+  # # index: show の補助ページ
+  # get 'messages', to: 'messages#index'
+
+  # # new: 新規作成用のフォームページ
+  # get 'messages/new', to: 'messages#new'
+
+  # # edit: 更新用のフォームページ
+  # get 'messages/:id/edit', to: 'messages#edit'
+
+  root to: 'messages#index' # トップページにアクセスしたときのルーティングを MessagesController の index アクションに設定
+  resources :messages
 end
